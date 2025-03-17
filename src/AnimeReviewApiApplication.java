af6b14645b6366ef404e6fa5eb1fe6d0d23fb754
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-03-17 19:54:59 +0200

    Added main application class for Spring Boot

---
+++ src/AnimeReviewApiApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeReviewApiApplication {
+    
+    public static void main (Spring[] args) {
+        SpringApplication.run(AnimeReviewApiApplication.class, args);
+    }
+}
