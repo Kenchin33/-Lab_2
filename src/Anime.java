8f0d7673b587c26b58364757419c1ab5d2c1519c
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-03-17 19:12:06 +0200

    Added Anime class

---
+++ src/Anime.java
@@ -0,0 +1,37 @@
+package com.example.animereviewapi;
+
+public class Anime {
+    private String id;
+    private String name;
+    private String genre;
+
+    public Anime(String id, String name, String genre){
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+} 
