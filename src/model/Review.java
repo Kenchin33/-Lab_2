35b872a3324d28526c5610e24528f796a0d4f436
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-03-17 19:26:46 +0200

    Added Review class

--- src/model/Review.java
+++ src/model/Review.java
@@ -1,5 +1,57 @@
-package com.example.animereviewapi;
+package model;
 
 public class Review {
-    
+    private String id;
+    private String animeId;
+    private String userId;
+    private String content;
+    private double rating;
+
+    public Review(String id, String animeId, String userId, String content, double rating) {
+        this.id = id;
+        this.animeId = animeId;
+        this.userId = userId;
+        this.content = content;
+        this.rating = rating;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAnimeId() {
+        return animeId;
+    }
+
+    public void setAnimeId(String animeId) {
+        this.animeId = animeId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
