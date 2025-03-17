949f293d0419cdbd26e6f7cf7b158def4c642eb8
Kenchin33 <169151335+Kenchin33@users.noreply.github.com>
2025-03-17 20:20:53 +0200

    Add 10 sample Anime and 10 Users in ReviewService

--- src/service/ReviewService.java
+++ src/service/ReviewService.java
@@ -44,4 +44,29 @@ public class ReviewService {
     public boolean deleteReview(String reviewId) {
         return reviews.removeIf(r -> r.getId().equals(reviewId));
     }
+
+
+    public ReviewService() {
+        animes.add(new Anime("1", "Naruto", "Action"));
+        animes.add(new Anime("2", "One Piece", "Adventure"));
+        animes.add(new Anime("3", "86", "Action"));
+        animes.add(new Anime("4", "Attack on Titan", "Action"));
+        animes.add(new Anime("5", "My Hero Academia", "Superhero"));
+        animes.add(new Anime("6", "Death Note", "Thriller"));
+        animes.add(new Anime("7", "One Punch Man", "Comedy"));
+        animes.add(new Anime("8", "Sword Art Online", "Sci-fi"));
+        animes.add(new Anime("9", "Your Lie in April", "Drama"));
+        animes.add(new Anime("10", "Blue Box", "Romance"));
+
+        users.add(new User("1", "Kenchin"));
+        users.add(new User("2", "Ipoha13"));
+        users.add(new User("3", "FisherMan"));
+        users.add(new User("4", "Tomura"));
+        users.add(new User("5", "Abdula"));
+        users.add(new User("6", "DramQueen"));
+        users.add(new User("7", "L"));
+        users.add(new User("8", "Kirito01"));
+        users.add(new User("9", "Sasuke_Back_In_Konoha"));
+        users.add(new User("10", "-------"));
+    }
 }
